be5ca0a283e25bb157faf282ddcae7096621a2f6
Yago Rodrigues <y.rodr1gues0811@gmail.com>
2024-01-12 16:40:21 -0300

    feat: creating project

---
+++ src/main/java/com/rodrigues/onepiecerestapi/OnepieceRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigues.onepiecerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepieceRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepieceRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigues/onepiecerestapi/OnepieceRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigues.onepiecerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepieceRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
