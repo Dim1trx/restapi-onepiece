6b1bb1e6c07a2cf5ef7155eea517f6a7e93eba22
Yago Rodrigues <y.rodr1gues0811@gmail.com>
2024-02-19 18:52:24 -0300

    feat (swagger): documenting both controllers

--- src/main/java/com/rodrigues/onepiecerestapi/controllers/CharacterController.java
+++ src/main/java/com/rodrigues/onepiecerestapi/controllers/CharacterController.java
@@ -3,6 +3,14 @@ package com.rodrigues.onepiecerestapi.controllers;
 import com.rodrigues.onepiecerestapi.model.character.CharacterDTO;
 import com.rodrigues.onepiecerestapi.model.character.ExposedCharacterDTO;
 import com.rodrigues.onepiecerestapi.services.CharacterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "api/v1/character")
 @CrossOrigin(origins = "*")
-
+@Tag(name = "Characters", description = "Endpoints for managing Characters entities")
 public class CharacterController {
     private final CharacterService service;
 
@@ -21,6 +29,21 @@ public class CharacterController {
 
     @GetMapping(
             produces = "application/json"
+    )
+    @Operation(
+            description = "Find all characters",
+            summary = "Find all characters",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = ExposedCharacterDTO.class)))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
     )
     public ResponseEntity<List<ExposedCharacterDTO>> findAll() {
         var list = service.findAll();
@@ -33,6 +56,23 @@ public class CharacterController {
             produces = "application/json"
     )
     @CrossOrigin(origins = "*")
+    @Operation(
+            description = "Find a specific character by its id",
+            summary = "Find a specific character by its id",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExposedCharacterDTO.class))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "Character id", required = true)
+            }
+    )
     public ResponseEntity<ExposedCharacterDTO> findById(@PathVariable long id) {
         return ResponseEntity.ok().body(service.findById(id));
     }
@@ -42,6 +82,28 @@ public class CharacterController {
             produces = "application/json"
     )
     @CrossOrigin(origins = "*")
+    @Operation(
+            description = "Create a new character",
+            summary = "Create a new character",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExposedCharacterDTO.class))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            },
+            requestBody = @RequestBody(
+                    description = "Character to be created",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CharacterDTO.class)
+                    )
+            )
+    )
     public ResponseEntity<ExposedCharacterDTO> create(@RequestBody CharacterDTO character) {
         return ResponseEntity.ok().body(service.create(character));
     }
@@ -50,11 +112,46 @@ public class CharacterController {
             consumes = "application/json",
             produces = "application/json"
     )
+    @Operation(
+            description = "Update a character by its id",
+            summary = "Update a character by its id",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json", schema = @Schema(implementation = ExposedCharacterDTO.class))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            },
+            requestBody = @RequestBody(
+                    description = "Character to be updated",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CharacterDTO.class)
+                    )
+            )
+    )
     public ResponseEntity<ExposedCharacterDTO> update(@RequestBody CharacterDTO character) {
         return ResponseEntity.ok().body(service.update(character));
     }
 
     @DeleteMapping( value = "/{id}")
+    @Operation(
+            description = "Delete a character by its id",
+            summary = "Delete a character by its id",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "Character id", required = true)
+            }
+    )
     public ResponseEntity<Void> delete(@PathVariable long id) {
         service.delete(id);
 
--- src/main/java/com/rodrigues/onepiecerestapi/controllers/CrewController.java
+++ src/main/java/com/rodrigues/onepiecerestapi/controllers/CrewController.java
@@ -2,6 +2,14 @@ package com.rodrigues.onepiecerestapi.controllers;
 
 import com.rodrigues.onepiecerestapi.model.crew.CrewDTO;
 import com.rodrigues.onepiecerestapi.services.CrewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "api/v1/crew")
 @CrossOrigin(origins = "*")
+@Tag(name = "Crew", description = "Endpoints for managing Crew entities")
 public class CrewController {
     private final CrewService service;
 
@@ -19,6 +28,21 @@ public class CrewController {
 
     @GetMapping(
             produces = "application/json"
+    )
+    @Operation(
+            description = "Find all crews",
+            summary = "Find all crews",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = CrewDTO.class)))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
     )
     public ResponseEntity<List<CrewDTO>> findAll() {
         var list = service.findAll();
@@ -30,6 +54,23 @@ public class CrewController {
             value = "/{id}",
             produces = "application/json"
     )
+    @Operation(
+            description = "Find a specific crew by its id",
+            summary = "Find a specific crew by its id",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json", schema = @Schema(implementation = CrewDTO.class))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "Crew id", required = true)
+            }
+    )
     public ResponseEntity<CrewDTO> findById(@PathVariable long id) {
         return ResponseEntity.ok().body(service.findById(id));
     }
@@ -39,6 +80,28 @@ public class CrewController {
             produces = "application/json"
     )
     @CrossOrigin(origins = "*")
+    @Operation(
+            description = "Create a new crew",
+            summary = "Create a new crew",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json", schema = @Schema(implementation = CrewDTO.class))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            },
+            requestBody = @RequestBody(
+                    description = "Crew to be created",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CrewDTO.class)
+                    )
+            )
+    )
     public ResponseEntity<CrewDTO> create(@RequestBody CrewDTO crew) {
         return ResponseEntity.ok().body(service.create(crew));
     }
@@ -47,11 +110,46 @@ public class CrewController {
             consumes = "application/json",
             produces = "application/json"
     )
+    @Operation(
+            description = "Update a crew by its id",
+            summary = "Update a crew by its id",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = "application/json", schema = @Schema(implementation = CrewDTO.class))
+                            }
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            },
+            requestBody = @RequestBody(
+                    description = "Crew to be updated",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CrewDTO.class)
+                    )
+            )
+    )
     public ResponseEntity<CrewDTO> update(@RequestBody CrewDTO crew) {
         return ResponseEntity.ok().body(service.update(crew));
     }
 
     @DeleteMapping( value = "/{id}")
+    @Operation(
+            description = "Delete a crew by its id",
+            summary = "Delete a crew by its id",
+            responses = {
+                    @ApiResponse(description = "Successful operation", responseCode = "200"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "Not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "Crew id", required = true)
+            }
+    )
     public ResponseEntity<Void> delete(@PathVariable long id) {
         service.delete(id);
 
