fc51ecdc5bac940c695e2c176b4ea58e8d847e62
Yago Rodrigues <y.rodr1gues0811@gmail.com>
2024-02-20 17:30:05 -0300

    feat: adding findByCrewId method to character repository

--- src/main/java/com/rodrigues/onepiecerestapi/repositories/CharacterRepository.java
+++ src/main/java/com/rodrigues/onepiecerestapi/repositories/CharacterRepository.java
@@ -4,6 +4,10 @@ import com.rodrigues.onepiecerestapi.model.character.Characters;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CharacterRepository extends JpaRepository<Characters, Long> {
+
+    public List<Characters> findByCrewId(Long crewId);
 }
